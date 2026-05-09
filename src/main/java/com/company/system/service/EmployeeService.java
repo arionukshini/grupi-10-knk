3cde44f20d5bb5ca922be463ab28571e036599ac
Arion <arionukshini01@gmail.com>
2026-05-09 21:02:27 +0200

    krijimi i EmployeeService

--- src/main/java/com/company/system/service/EmployeeService.java
+++ src/main/java/com/company/system/service/EmployeeService.java
@@ -1,4 +1,64 @@
 package com.company.system.service;
 
+import com.company.system.db.DBConnection;
+import com.company.system.model.Employee;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeService {
+
+    public static List<Employee> getAllEmployees() {
+
+        List<Employee> employees = new ArrayList<>();
+
+        String sql = "SELECT * FROM employees";
+
+        try (
+                Connection conn = DBConnection.connect();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()
+        ) {
+
+            while (rs.next()) {
+
+                Employee employee = new Employee(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("position"),
+                        rs.getDouble("salary")
+                );
+
+                employees.add(employee);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return employees;
+    }
+
+    public static void addEmployee(Employee employee) {
+
+        String sql = "INSERT INTO employees(name, position, salary) VALUES (?, ?, ?)";
+
+        try (
+                Connection conn = DBConnection.connect();
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+
+            stmt.setString(1, employee.getName());
+            stmt.setString(2, employee.getPosition());
+            stmt.setDouble(3, employee.getSalary());
+
+            stmt.executeUpdate();
+
+            System.out.println("Employee added successfully!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,13 @@ module com.company.system {
     requires javafx.fxml;
     requires java.sql;
 
-    opens com.company.system to javafx.fxml;
-    opens com.company.system.ui to javafx.fxml;
-
     exports com.company.system;
     exports com.company.system.model;
+    exports com.company.system.service;
+    exports com.company.system.db;
+
+    opens com.company.system to javafx.fxml;
+    opens com.company.system.ui to javafx.fxml;
     opens com.company.system.model to javafx.fxml;
+    opens com.company.system.controller to javafx.fxml;
 }
