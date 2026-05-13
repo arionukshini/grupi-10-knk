6ba0ceec0302d0dddea6207a113e6370d7e3c68f
Arion <arionukshini01@gmail.com>
2026-05-13 21:11:18 +0200

    Inicializimi i databazes ne ekzekutim

--- src/main/java/com/company/system/MainApp.java
+++ src/main/java/com/company/system/MainApp.java
@@ -1,6 +1,6 @@
 package com.company.system;
 
-import com.company.system.ui.MainLayout;
+import com.company.system.db.DBConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -20,6 +20,7 @@ public class MainApp extends Application {
 
         primaryStage.setTitle("Contract & Payroll System");
 
+        DBConnection.initializeDatabase();
         showLogin();
     }
 
