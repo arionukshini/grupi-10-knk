cdaf22fc1eb591b4ded0ce3a80c3ee84dc157968
arjanitalestrani1 <arjanita.lestrani@student.uni-pr.edu>
2026-05-16 17:44:55 +0200

    Translate main toolbar buttons

--- src/main/java/com/company/system/controller/MainController.java
+++ src/main/java/com/company/system/controller/MainController.java
@@ -3,6 +3,7 @@ package com.company.system.controller;
 import com.company.system.i18n.LanguageManager;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
@@ -45,6 +46,18 @@ public class MainController {
     private MenuItem helpMenuItem;
 
     @FXML
+    private Button employeesButton;
+
+    @FXML
+    private Button contractsButton;
+
+    @FXML
+    private Button salariesButton;
+
+    @FXML
+    private Button dashboardButton;
+
+    @FXML
     private StackPane contentArea;
 
     @FXML
@@ -70,6 +83,11 @@ public class MainController {
         salariesMenuItem.setText(LanguageManager.get("menu.salaries"));
         dashboardMenuItem.setText(LanguageManager.get("menu.dashboard"));
         helpMenuItem.setText(LanguageManager.get("menu.help"));
+
+        employeesButton.setText(LanguageManager.get("menu.employees"));
+        contractsButton.setText(LanguageManager.get("menu.contracts"));
+        salariesButton.setText(LanguageManager.get("menu.salaries"));
+        dashboardButton.setText(LanguageManager.get("menu.dashboard"));
 
         welcomeLabel.setText(LanguageManager.get("app.welcome"));
 
