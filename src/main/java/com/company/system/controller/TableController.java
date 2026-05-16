d76e43db15c0bf9d2040e035b1d9760ba1b8c001
FlorentinaDervishaj <florentina.dervishaj@student.uni-pr.edu>
2026-05-16 21:18:09 +0200

    Kalova logjiken e databazes ne EmployeeService

--- src/main/java/com/company/system/controller/TableController.java
+++ src/main/java/com/company/system/controller/TableController.java
@@ -1,6 +1,5 @@
 package com.company.system.controller;
 
-import com.company.system.db.DBConnection;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -8,11 +7,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import com.company.system.model.Employee;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import javafx.beans.property.SimpleStringProperty;
+import com.company.system.service.EmployeeService;
 
 
 public class TableController {
@@ -45,8 +41,7 @@ public class TableController {
 
         employeeTable.setPlaceholder(new javafx.scene.control.Label("No employees found"));
 
-        loadEmployeesFromDatabase();
-
+        loadEmployees();
 
         employeeTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
             if (newSelection != null) {
@@ -56,47 +51,12 @@ public class TableController {
     }
     @FXML
     private void handleRefresh() {
-        loadEmployeesFromDatabase();
+        loadEmployees();
         System.out.println("Table refreshed.");
     }
-
-    private void loadEmployeesFromDatabase() {
-        ObservableList<Employee> employees = FXCollections.observableArrayList();
-        String query = "SELECT * FROM employees";
-
-        Connection connection = DBConnection.connect();
-
-        if (connection == null) {
-            System.out.println("Connection failed!");
-            return;
-        }
-
-
-            try (Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(query))  {
-
-            while (resultSet.next()) {
-                Employee employee = new Employee(
-                        resultSet.getInt("id"),
-                        resultSet.getString("first_name"),
-                        resultSet.getString("last_name"),
-                        resultSet.getString("email"),
-                        resultSet.getString("phone"),
-                        resultSet.getString("position"),
-                        resultSet.getInt("department_id"),
-                        resultSet.getDate("hire_date"),
-                        resultSet.getDouble("base_salary"),
-                        resultSet.getString("status")
-                );
-
-                employees.add(employee);
-            }
-
-            employeeTable.setItems(employees);
-
-        } catch (SQLException e) {
-                System.out.println("Error loading employees: " + e.getMessage());
-            }
+    private void loadEmployees() {
+        ObservableList<Employee> employees = FXCollections.observableArrayList(EmployeeService.getAllEmployees());
+        employeeTable.setItems(employees);
     }
 }
 
