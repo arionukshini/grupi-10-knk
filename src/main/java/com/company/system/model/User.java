88da0d2a31c7382d9a52d0525b2674475f345bde
Edison <edison.ukshini@student.uni-pr.edu>
2026-05-17 18:16:57 +0200

    tregon kur eshte kriju llogaria

--- src/main/java/com/company/system/model/User.java
+++ src/main/java/com/company/system/model/User.java
@@ -1,14 +1,19 @@
 package com.company.system.model;
 
+import java.sql.Timestamp;
+
 public class User {
+
     private final int id;
     private final String username;
     private final String password;
+    private final Timestamp createdAt;
 
-    public User(int id, String username, String password) {
+    public User(int id, String username, String password, Timestamp createdAt) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.createdAt = createdAt;
     }
 
     public int getId() {
@@ -22,4 +27,6 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public Timestamp getCreatedAt() {return createdAt;}
 }
