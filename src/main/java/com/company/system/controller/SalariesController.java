24e8e5820e571468cb672cdd399aa2e580e9f1d3
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-05-24 15:55:42 +0200

    Shtimi i imports per salaries controller

--- src/main/java/com/company/system/controller/SalariesController.java
+++ src/main/java/com/company/system/controller/SalariesController.java
@@ -1,5 +1,25 @@
 package com.company.system.controller;
 
+import com.company.system.model.Salary;
+import com.company.system.service.SalaryService;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+
 public class SalariesController {
     private final ObservableList<Salary> salaries =
             FXCollections.observableArrayList();
@@ -8,5 +28,7 @@ public class SalariesController {
             FXCollections.observableArrayList();
 
     private Salary calculatedSalary;
+
+
 
 }
