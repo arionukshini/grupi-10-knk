23f41512a508dfe2812654177cbac8a0df163861
Arion <arionukshini01@gmail.com>
2026-05-28 14:17:36 +0200

    perditesimi i alert per exceptions

--- src/main/java/com/company/system/exceptions/InvalidEmailException.java
+++ src/main/java/com/company/system/exceptions/InvalidEmailException.java
@@ -1,5 +1,6 @@
 package com.company.system.exceptions;
 
+import com.company.system.utils.DialogUtils;
 import javafx.scene.control.Alert;
 
 public class InvalidEmailException extends RuntimeException {
@@ -10,6 +11,7 @@ public class InvalidEmailException extends RuntimeException {
 
     public void showInvalidEmailExceptionAlert(String email, String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
+        DialogUtils.style(alert);
         alert.setHeaderText(null);
         alert.setContentText(message + " " + email + " is not a valid email!");
         alert.setTitle("Gabim");
--- src/main/java/com/company/system/exceptions/InvalidSalaryException.java
+++ src/main/java/com/company/system/exceptions/InvalidSalaryException.java
@@ -1,5 +1,6 @@
 package com.company.system.exceptions;
 
+import com.company.system.utils.DialogUtils;
 import javafx.scene.control.Alert;
 
 public class InvalidSalaryException extends RuntimeException {
@@ -10,6 +11,7 @@ public class InvalidSalaryException extends RuntimeException {
 
     public void showInvalidSalaryExceptionAlert(double salary, String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
+        DialogUtils.style(alert);
         alert.setHeaderText(null);
         alert.setContentText(message + " " + salary + " is not a valid salary!");
         alert.setTitle("Gabim");
